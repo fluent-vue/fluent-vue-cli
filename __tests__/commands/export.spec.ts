00cc23e3234c0f4217f897e2ae39041b067f7739
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-10-08 15:40:41 +0300

    Add snapshot testing for extracted files

--- __tests__/commands/export.spec.ts
+++ __tests__/commands/export.spec.ts
@@ -1,6 +1,36 @@
 import { sync as rimraf } from 'rimraf'
 
+import fs from 'fs'
+import path from 'path'
+
 import ExportCommand from '../../src/commands/export'
+
+interface FileInfo {
+  filepath: string
+  content: string
+}
+
+/**
+ * Read files synchronously from a folder, with natural sorting
+ * @param dir Absolute path to directory
+ * @returns File content
+ */
+function readFilesSync (dir: string): FileInfo[] {
+  return fs.readdirSync(dir)
+    .flatMap(filename => {
+      const filepath = path.resolve(dir, filename)
+      const stat = fs.statSync(filepath)
+      const isFile = stat.isFile()
+
+      if (isFile) {
+        const content = fs.readFileSync(filepath).toString()
+
+        return { filepath: path.relative(process.cwd(), filepath), content }
+      }
+
+      return readFilesSync(filepath)
+    })
+}
 
 describe('export', () => {
   let result: string | Uint8Array | undefined
@@ -27,5 +57,9 @@ describe('export', () => {
 
     // Assert
     expect(result).toContain('Extracted messages from 2 files')
+
+    readFilesSync('translations').forEach(file => {
+      expect(file.content).toMatchSnapshot(file.filepath)
+    })
   })
 })
