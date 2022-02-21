7e801c0ebeb7711454105410db80d8ea821126bc
Eugene Kovalchuk <kovalchukq@gmail.com>
2022-02-21 16:06:20 +0200

    Implemented import command (#10)
    Co-authored-by: INTERLOGIC\yeko <yeko@interlogic.com.ua>

--- src/cli/commands/export.ts
+++ src/cli/commands/export.ts
@@ -1,29 +1,27 @@
 import { stream } from 'fast-glob'
 import { promises as fs, existsSync } from 'fs'
-import { resolve, dirname } from 'path'
-
+import {resolve, dirname, join, relative} from 'path'
+import unixify from 'unixify'
 import { getVueMessages, mergeFtl } from '../..'
 
 interface Options {
+  inDir: string
   outDir: string
   clean: boolean
 }
 
 const log = console.log.bind(console)
 
-export const run = async (argv: any[], flags: Options) => {
+export const run = async (flags: Options) => {
   let count = 0
-  for await (const file of stream(argv)) {
+  for await (const file of stream(unixify(flags.inDir) + '/**', {ignore: ['**/node_modules']})) {
     count ++
     const data = await fs.readFile(file)
 
     const vueMessages = getVueMessages(data.toString())
-
     for (const { locale, source, messages } of vueMessages) {
-      const outputPath = resolve(flags.outDir, locale, `${file}.ftl`)
-
+      const outputPath = join(flags.outDir, locale, relative(flags.inDir, `${file}.ftl`))
       await fs.mkdir(dirname(outputPath), { recursive: true })
-
       if (flags.clean || !existsSync(outputPath)) {
         await fs.writeFile(outputPath, source)
       } else {
